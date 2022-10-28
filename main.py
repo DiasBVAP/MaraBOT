ac2d954b3459145cf3093476b3431bfb44ca647d
Bruno Dias <47839876+DiasBVAP@users.noreply.github.com>
2022-10-28 18:18:03 -0300

    Moved token to .env and ping on ready

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import discord
+import os
+from dotenv import load_dotenv
 
 class MyClient(discord.Client):
+    MAIN_CHANNEL = 538755995611168772
     async def on_ready(self):
         print(f'Logged on as {self.user}!')   
+        for member in self.get_all_members():
+            if ((member.name == 'Maragamer') and (member.status == discord.Status.online)):
+                channel = client.get_channel(self.MAIN_CHANNEL)
+                await channel.send(f'Cheguei. Me mama {member.mention}')
+                
 
     async def on_presence_update(self, before, after):
-        if (after.name == 'Maragamer' and after.status == discord.Status.online):
-            channel = client.get_channel(538755995611168772)
+        if ((after.name == 'Maragamer') and (after.status == discord.Status.online)):
+            channel = client.get_channel(self.MAIN_CHANNEL)
             await channel.send(f'Me mama {after.mention}')
 
 intents = discord.Intents.all()
 client = MyClient(intents=intents)
-client.run()
+
+load_dotenv()
+token = os.environ.get('TOKEN')
+client.run(token)
---
+++ start.bat
@@ -0,0 +1,3 @@
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
+	python main.py
+exit
