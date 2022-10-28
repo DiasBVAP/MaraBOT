44c34c99ecd7f07fd7427c72508ecfe41e92b06d
Bruno Dias <47839876+DiasBVAP@users.noreply.github.com>
2022-10-28 19:04:31 -0300

    Generalize and update readme

--- main.py
+++ main.py
@@ -3,23 +3,25 @@ import os
 from dotenv import load_dotenv
 
 class MyClient(discord.Client):
-    MAIN_CHANNEL = 538755995611168772
     async def on_ready(self):
+        load_dotenv()
         print(f'Logged on as {self.user}!')   
         for member in self.get_all_members():
-            if ((member.name == 'Maragamer') and (member.status == discord.Status.online)):
-                channel = client.get_channel(self.MAIN_CHANNEL)
-                await channel.send(f'Cheguei. Me mama {member.mention}')
+            if ((member.name == os.environ.get('USER_NAME')) and (member.status == discord.Status.online)):
+                channel = client.get_channel(int(os.environ.get('CHANNEL_ID')))
+                await channel.send(member.mention)
                 
 
     async def on_presence_update(self, before, after):
-        if ((after.name == 'Maragamer') and (after.status == discord.Status.online)):
-            channel = client.get_channel(self.MAIN_CHANNEL)
-            await channel.send(f'Me mama {after.mention}')
+        load_dotenv()
+        if ((after.name == os.environ.get('USER_NAME')) and (after.status == discord.Status.online)):
+            channel = client.get_channel(int(os.environ.get('CHANNEL_ID')))
+            await channel.send(after.mention)
+
+load_dotenv()
+token = os.environ.get('TOKEN')
 
 intents = discord.Intents.all()
 client = MyClient(intents=intents)
 
-load_dotenv()
-token = os.environ.get('TOKEN')
 client.run(token)
--- readme.md
+++ readme.md
@@ -1,1 +1,16 @@
 # MaraBOT
+
+This bot's sole purpose is mentioning someone when they get online on Discord.
+
+## Installation
+
+Just clone the repo and run ```pip install -r requirements.txt```.
+
+## Setup
+
+In *.env*, add your bot's token, the id of the channel that you want the mentions to be sent on,
+and the Discord id (minus the tag. Ex: John#9999 -> John) of the user you want to be mentioned.
+
+## Usage
+
+Start the script by running *start.bat*. If you want to run the bot on startup, just add an shortcut to *start.bat* to your windows startup folder.
